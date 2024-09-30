d3a4d248f7bf4f2b10ac07a44aeefa7668057766
Patrick <rautraut.john19@gmail.com>
2024-09-30 22:05:42 +0800

    fb auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:eggventure/firebase_options.dart';
 import 'package:eggventure/routes/routes.dart';
 import 'package:eggventure/screens/home_screen.dart';
 import 'package:eggventure/screens/order_screen.dart';
@@ -5,16 +6,24 @@ import 'package:eggventure/screens/signup_screen.dart';
 import 'package:eggventure/splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
       statusBarColor: Colors.white, statusBarIconBrightness: Brightness.dark));
+  try {
+    await Firebase.initializeApp(
+        options: DefaultFirebaseOptions.currentPlatform);
+    debugPrint("Firebase initialized Successfully");
+  } catch (e) {
+    debugPrint("Failed to initialized: $e");
+  }
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
