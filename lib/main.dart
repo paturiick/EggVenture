b9ba733fd977e9704ecf16849ad9bd7fa1208d4e
Bobert <salvo.robertroy@gmail.com>
2024-09-30 20:45:49 +0800

    feat: setup firebase auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,25 @@
+import 'package:eggventure/firebase_options.dart';
 import 'package:eggventure/routes/routes.dart';
 import 'package:eggventure/screens/home_screen.dart';
 import 'package:eggventure/screens/order_screen.dart';
 import 'package:eggventure/screens/signup_screen.dart';
 import 'package:eggventure/splash_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-void main() {
+void main() async {
   SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
       statusBarColor: Colors.white, statusBarIconBrightness: Brightness.dark));
+    WidgetsFlutterBinding.ensureInitialized();
+  
+  try {
+    await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+    print("Firebase Initialized Successfully");
+  } catch (e) {
+    print("Error initializing Firebase: $e");
+  }
+  
   runApp(const MyApp());
 }
 
